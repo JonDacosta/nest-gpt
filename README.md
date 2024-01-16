567bd637a1fcdcbe07c150633833ce32e99add68
Jonatan Dacosta <dacostaafonso@gmail.com>
2024-01-16 12:12:23 +0100

    Base del proyecto

---
+++ README.md
@@ -0,0 +1,39 @@
+## Descripción
+
+1. Clonar el repositorio
+
+## Instalación
+
+```bash
+$ npm install
+```
+
+## Crear un archivo .env basado en .env.template
+
+## Ejecutar la App
+
+```bash
+# development
+$ npm run start
+
+# watch mode
+$ npm run start:dev
+
+# production mode
+$ npm run start:prod
+```
+
+## Test
+
+```bash
+# unit tests
+$ npm run test
+
+# e2e tests
+$ npm run test:e2e
+
+# test coverage
+$ npm run test:cov
+```
+
+
---
+++ src/gpt/gpt.controller.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Post } from '@nestjs/common';
+import { GptService } from './gpt.service';
+import { OrthographyDto } from './dtos';
+
+@Controller('gpt')
+export class GptController {
+  constructor(private readonly gptService: GptService) {}
+
+
+  @Post('orthography-check')
+  orthographyCheck(
+    @Body() orthographyDto: OrthographyDto,
+  ) {
+    // return orthographyDto;
+
+    return this.gptService.orthographyCheck(orthographyDto);
+  }
+}
