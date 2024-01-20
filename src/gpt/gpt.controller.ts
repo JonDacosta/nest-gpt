5289f4743bd1fc3af4742a4a4a5a8bab0a645055
Jonatan Dacosta <dacostaafonso@gmail.com>
2024-01-20 13:23:47 +0100

    add - stream mode y pros/cons

--- src/gpt/gpt.controller.ts
+++ src/gpt/gpt.controller.ts
@@ -1,7 +1,8 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
 import { GptService } from './gpt.service';
-import { OrthographyDto } from './dtos';
+import { OrthographyDto, ProsConsDiscusserDto } from './dtos';
+import { Response } from 'express';
 
 @Controller('gpt')
 export class GptController {
@@ -15,5 +16,31 @@ export class GptController {
     // return orthographyDto;
 
     return this.gptService.orthographyCheck(orthographyDto);
+  }
+
+  @Post('pros-cons-discusser')
+  prosConsDiscusser(
+    @Body() prosConsDiscusserDto: ProsConsDiscusserDto,
+  ) {
+    return this.gptService.prosConsDiscusser(prosConsDiscusserDto)
+  }
+
+  @Post('pros-cons-discusser-stream')
+  async prosConsDiscusserStream(
+    @Body() prosConsDiscusserDto: ProsConsDiscusserDto,
+    @Res() res: Response,
+  ) {
+    const stream = await this.gptService.prosConsDiscusserStream(prosConsDiscusserDto);
+    
+    res.setHeader('Content-Type', 'application/json');
+    res.status( HttpStatus.OK );
+
+    for await( const chunk of stream ) {
+      const piece = chunk.choices[0].delta.content || ''; 
+      console.log(piece) 
+      res.write(piece);
+    }
+
+    res.end();
   }
 }
--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -3,8 +3,8 @@ import { Injectable } from '@nestjs/common';
 
 import OpenAI from 'openai';
 
-import { orthographyCheckUseCase } from './use-cases';
-import { OrthographyDto } from './dtos';
+import { orthographyCheckUseCase, prosConsDiscusserStreamUseCase, prosConsDiscusserUseCase } from './use-cases';
+import { OrthographyDto, ProsConsDiscusserDto } from './dtos';
 
 @Injectable()
 export class GptService {
@@ -19,5 +19,16 @@ export class GptService {
             prompt: orthographyDto.prompt,
         });
 
+    }
+    async prosConsDiscusser(prosConsDiscusserDto: ProsConsDiscusserDto) {
+        return await prosConsDiscusserUseCase(this.openai, {
+            prompt: prosConsDiscusserDto.prompt
+        });
+    }
+
+    async prosConsDiscusserStream(prosConsDiscusserDto: ProsConsDiscusserDto) {
+        return await prosConsDiscusserStreamUseCase(this.openai, {
+            prompt: prosConsDiscusserDto.prompt
+        });
     }
 }
